4fbc9fbc416f5d671c89069705cb48da8ad8e1cc
Do Hoang Anh Vu <Dkrusvan18091985@gmail.com>
2024-08-26 21:08:29 +0700

    video 30

--- src/component/learn/MyComponent.jsx
+++ src/component/learn/MyComponent.jsx
@@ -1,9 +1,15 @@
 // () => { }
 // component = html + css + js
+// JSX
+//Fragment <> </ >
+import './style.css';
+
 const MyComponent = () =>{
     return (
-      <div>Đỗ Hoàng Anh Vũ</div>
-      
+      <>
+        <div className="MyName" style={{ borderRadius: "10px" }}
+        >Đỗ Hoàng Anh Vũ</div>
+      </>
     );
 }
 export default MyComponent;
