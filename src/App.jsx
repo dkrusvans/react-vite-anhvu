6ccf281c445610ab1ee4318dc953d37c36b98cce
Do Hoang Anh Vu <Dkrusvan18091985@gmail.com>
2024-08-26 19:28:30 +0700

    video 27

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+const App = () => {
   const [count, setCount] = useState(0)
 
-  return (
+  return ( 
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
