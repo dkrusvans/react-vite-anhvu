2bed6a9c87ece851a1bf50610404e3919f56444c
Do Hoang Anh Vu <Dkrusvan18091985@gmail.com>
2024-08-27 20:16:18 +0700

    video 34

--- src/component/todo/TodoNew.jsx
+++ src/component/todo/TodoNew.jsx
@@ -1,11 +1,23 @@
 const TodoNew = (props) => {
   console.log(">>> check point: ", props)
   const { AddNewTodo } = props;
-  AddNewTodo("Anh vu")
+  // AddNewTodo("Anh vu")
+  const handleClick = () => {
+    alert("Click Me")
+  }
+  const handleOnChange = () => {
+    console.log("handleOnChange")
+  }
     return (
         <div className='todo-new'>
-          <input type="text" />
-          <button>Add</button>
+        <input type="text"
+          onChange={handleOnChange}
+
+        />
+        <button
+          style={{ cursor: "pointer" }}
+          onClick={handleClick}
+        >Add</button>
         </div>
     )
 }
