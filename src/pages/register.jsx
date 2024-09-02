ac767d0782c821ec63a47ec4493e7ee4f35dd8bc
Do Hoang Anh Vu <Dkrusvan18091985@gmail.com>
2024-09-02 11:24:31 +0700

    video 90

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,7 +1,140 @@
+import { Button, Form, Input, message, notification } from "antd";
+import { registerUserAPI } from "../services/api.service";
+import { useNavigate } from "react-router-dom";
 
-const Register = () => {
+const RegisterPage = () => {
+    const [form] = Form.useForm();
+    const navigate = useNavigate();
+
+    const onFinish = async (values) => {
+        console.log("check value", values)
+        //call APIvalues.
+        const res = await registerUserAPI(
+            values.fullName,
+            values.email,
+            values.passWords,
+            values.phoneNumber);
+        if (res.data) {
+            notification.success({
+                message: "Register User",
+                description: "Dang ky user thanh cong !"
+            })
+            
+            navigate("/login");
+        } else {
+            notification.error({
+                message: "Register User Error",
+                description:JSON.stringify(res.message)
+            })
+        }
+    }
     return (
-        <div>register page</div>
+        <Form
+            form={form}
+            layout="vertical"
+            // name="basic"
+            // labelCol={{
+            // span: 8,
+            // }}
+            // wrapperCol={{
+            // span: 16,
+            // }}
+            // style={{
+            // maxWidth: 600,
+            // }}
+            
+            onFinish={onFinish}
+            // onFinishFailed={onFinishFailed}
+            // autoComplete="off"
+        >
+
+        <div style={{
+            margin: "50px",
+            
+            }}>
+            
+                {/* <Form.Item
+                label="Username"
+                name="username"
+                // rules={[
+                //     {
+                //     required: true,
+                //     message: 'Please input your username!',
+                //     },
+                // ]}
+                >
+                    <Input />
+                </Form.Item> */}
+                <Form.Item
+                label="Full Name"
+                name="fullName"
+                rules={[
+                    {
+                    required: true,
+                    message: 'Please input your fullname!',
+                    },
+                ]}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                label="Email"
+                name="email"
+                rules={[
+                    {
+                    required: true,
+                    message: 'Please input your Email!',
+                    },
+                ]}
+                >
+                    <Input />
+                
+                </Form.Item>
+                <Form.Item
+                label="Passworld"
+                name="passWords"
+                rules={[
+                    {
+                    required: true,
+                    message: 'Please input your Passworld!',
+                    },
+                ]}
+                >
+                    <Input.Password/>
+                
+                </Form.Item>
+                <Form.Item
+                label="Phone Number"
+                name="phoneNumber"
+                rules={[
+                    {
+                    required: true,
+                    // type: "regexp",
+                    pattern: new RegExp(/\d+/g),
+                    message: 'Wrong format!',
+                    },
+                ]}
+                >
+                    <Input />
+                
+                </Form.Item>
+                
+                
+            <div>
+                    <Button onClick={() => form.submit()}
+                        type="primary">Register</Button>
+                    
+                    {/* tu field data */}
+                    {/* <Button onClick={() => { 
+                        form.setFieldsValue({
+                            email: "dkrusvan18091985@gmail.com"
+                        })
+                        
+                    }}>Test</Button> */}
+                
+            </div>
+            </div>
+        </Form>
     )
 }
-export default Register;
+export default RegisterPage;
--- src/services/api.service.js
+++ src/services/api.service.js
@@ -11,6 +11,7 @@ const createUserAPI=(fullName,email,passWords,phoneNumber)=>{
         }
     return axios.post(URL_BACKEND, data);
 }
+
 const updateUserAPI=(_id,fullName,phoneNumber)=>{
     const URL_BACKEND="/api/v1/user";
     const data = {
@@ -20,15 +21,18 @@ const updateUserAPI=(_id,fullName,phoneNumber)=>{
     }
     return axios.put(URL_BACKEND, data);
 }
+
 const fetchAllUserAPI = (current, pageSize) => {
     const URL_BACKEND=`/api/v1/user?current=${current}&pageSize=${pageSize}`;
         
     return axios.get(URL_BACKEND);
 }
+
 const deleteUserAPI = (id) => {
     const URL_BACKEND = `/api/v1/user/${id}`;
     return axios.delete(URL_BACKEND);
 }
+
 const handleUploadFile = (file, folder) => {
     const URL_BACKEND = `/api/v1/file/upload`;
     let config = {
@@ -41,6 +45,7 @@ const handleUploadFile = (file, folder) => {
     bodyFormData.append("fileImg", file)
     return axios.post(URL_BACKEND, bodyFormData, config);
 }
+
 const updateUserAvatarAPI =(avatar, _id, fullName, phoneNumber)=>{
     const URL_BACKEND="/api/v1/user";
     const data = {
@@ -51,7 +56,19 @@ const updateUserAvatarAPI =(avatar, _id, fullName, phoneNumber)=>{
     }
     return axios.put(URL_BACKEND, data);
 }
+
+const registerUserAPI=(fullName,email,passWords,phoneNumber)=>{
+    const URL_BACKEND="/api/v1/user/register";
+        const data={
+            fullName:fullName,
+            email: email,
+            password: passWords,
+            phone: phoneNumber
+        }
+    return axios.post(URL_BACKEND, data);
+}
+
 export {
     createUserAPI, updateUserAPI, fetchAllUserAPI,
-    deleteUserAPI, handleUploadFile, updateUserAvatarAPI
+    deleteUserAPI, handleUploadFile, updateUserAvatarAPI, registerUserAPI
 }
